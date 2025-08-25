7f89a7c932aa7df87e0d0a81ae669a9643048856
JAKlein26 <117413359+JAKlein26@users.noreply.github.com>
2025-08-24 23:07:20 -0700

    finished do something example project people can use to learn how to use android studio

--- app/src/main/java/com/do_something_scouting_app/MainFragment.java
+++ app/src/main/java/com/do_something_scouting_app/MainFragment.java
@@ -6,16 +6,33 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.do_something_scouting_app.databinding.MainFragmentBinding;
 
 public class MainFragment extends Fragment {
     MainFragmentBinding binding;
 
+    //this is the variable that tracks what number is on the example button
+    int exampleButtonValue = 0;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         this.binding = MainFragmentBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //This is an example of what code looks like to make a button increase every time you press it
+        binding.exampleButton.setOnClickListener(view1 -> {
+            //this line increases the value of the "exampleButtonValue" variable by 1
+            exampleButtonValue++;
+            //this line sets the text of the "exampleButton" to the "exampleButtonValue"
+            binding.exampleButton.setText(String.valueOf(exampleButtonValue));
+        });
     }
 }
